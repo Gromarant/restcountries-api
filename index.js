dcb08c1a871559a05026a53cf68b7d871cd18773
Mariangelica Rodriguez <gromarant@gmail.com>
2023-05-14 01:36:56 +0200

    add request url and data treatment

--- index.js
+++ index.js
@@ -7,17 +7,22 @@ const cardTemplate = function (/* You can pass the data here*/) {
 
 const countriesNode = document.getElementById("countries");
 
-fetch(/* Need the provide API URL to get all countries */)
-  .then(function (response) {
-    // fetch() returns a promise containing the response (a Response object).
-    // This is just an HTTP response, not the actual JSON. 
-    // To extract the JSON body content from the response, 
-    // we use the json() method and pass it into the next .then()
-  })
-  .then(function (countries) {
-    // Here is where you'll need to add into the DOM all the countries received from API 
-
-    // 1 - We will need to iterate the countries variable with a loop
-    // 2 - You can use the cardTemplate() function to create a div with a class card already styled
-    // 💡 you can use countriesNode variable to add elements
-  });
+try {
+  fetch(`https://restcountries.com/v3.1/all`)
+    .then(function (response) {
+      let data = response.json();
+      return data;
+    })
+    .then(function (countries) {
+      countries.map(country => {
+        let name = country.name.common;
+        let flag = country.flags.svg;
+        let alt = country.flags.alt;
+  
+        countriesNode.innerHTML += cardTemplate({name, flag, alt});
+      })
+    });
+}
+catch (error) {
+  console.log(`Error: error.stack`);
+}
